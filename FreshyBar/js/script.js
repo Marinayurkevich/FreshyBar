e2e5a50d22f48f69394a44e0c4b3048a07a82802
Marina Yurkevich <roxy-lo@inbox.ru>
2023-08-11 19:24:39 +0300

    feat: add scroll behavior

--- FreshyBar/js/script.js
+++ FreshyBar/js/script.js
@@ -26,6 +26,31 @@ const createCard = (item) => {
     return cocktail;
 }
 
+const scrollService = {
+    scrollPosition: 0,
+    disabledScroll() {
+        this.scrollPosition = window.scrollY;
+        document.documentElement.style.scrollBehavior = 'auto';
+        // console.log(this.scrollPosition);
+        document.body.style.cssText = `
+        overflow: hidden;
+        position: relative;        
+        top: -${this.scrollPosition}px;
+        left: 0;
+        height: 100vh;
+        weight: 100vw;
+        padding-right: ${window.innerWidth - document.body.offsetWidth}px;
+        `;
+    },
+    enabledScroll() {
+        document.body.style.cssText = "";
+        window.scroll({ top: this.scrollPosition });
+        // documentElement это html
+        document.documentElement.style.scrollBehavior = "";
+    },
+};
+
+
 const modalController = ({ modal, btnOpen, time }) => {
     const buttonElem = document.querySelector(btnOpen);
     const modalElem = document.querySelector(modal);
@@ -35,31 +60,35 @@ const modalController = ({ modal, btnOpen, time }) => {
     visibility: hidden;
     opacity: 0;
     transition: opacity ${time}ms ease-in-out;
-    `
+    `;
 
     const closeModal = (event) => {
+        // нужен target - при нажатии на фон будет закрываться модальное окно, внутри окна можно будет работать (оно не будет закрываться при нажатии на элементы внутри окна)
         const target = event.target;
         const code = event.code;
+        // console.log(code);
+        // code - клавиша, которая нажата на клавиатуре
         if (target === modalElem || code === "Escape") {
             modalElem.style.opacity = 0;
             setTimeout(() => {
                 modalElem.style.visibility = "hidden";
-            }, time)
-            window(removeEventListener('keydown', closeModal))
+                scrollService.enabledScroll();
+            }, time);
+            window.removeEventListener('keydown', closeModal);
         }
     }
-
 
     const openModal = () => {
         modalElem.style.visibility = "visible";
         modalElem.style.opacity = 1;
-        window(addEventListener('keydown', closeModal))
-    }
-
+        window.addEventListener('keydown', closeModal);
+        scrollService.disabledScroll();
+    };
 
     buttonElem.addEventListener('click', openModal);
     modalElem.addEventListener('click', closeModal);
 
+    // возвращаем объект, т.к. если прсто перечислить функции, то вернется только последняя
     return { openModal, closeModal };
 }
 
@@ -67,17 +96,25 @@ const modalController = ({ modal, btnOpen, time }) => {
 
 
 const init = async () => {
+    //корзина
+    // при нажатии на кнопку с классом header__btn-order, открывается элемент с классом modal_order
     modalController({ modal: '.modal_order', btnOpen: '.header__btn-order' });
+
+
     const goodsListElem = document.querySelector(".goods__list");
     const data = await getData();
     console.log(data);
+
     const cartsCocktail = data.map((item) => {
         const li = document.createElement('li');
         li.classList.add("goods__item");
+        // li.textContent = item.title;
         li.append(createCard(item));
         return li;
     });
+
     goodsListElem.append(...cartsCocktail);
 }
 
 init();
+
